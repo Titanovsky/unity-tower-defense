8dd537e7702cd691c09563379973069ef754a522
Titanovsky <titanovskyteam@gmail.com>
2024-05-15 22:11:06 +0500

    add EventBus skeleton (without the reliasition)

---
+++ Assets/Scripts/PatternExamples/EventBus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class EventBus
+{
+	public void AddEvent(Delegate d)
+	{
+		//todo
+	}
+
+	public void Subscribe()
+	{
+
+	}
+
+	public void Unsubscribe() 
+	{ 
+	}
+
+	public void Invoke()
+	{
+
+	}
+}
