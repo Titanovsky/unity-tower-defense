108865ad580f68c0bf687d06e1e5dbc97856bf33
Titanovsky <titanovskyteam@gmail.com>
2024-05-16 18:03:22 +0500

    add non-work WaveHandler

---
+++ Assets/Scripts/System/WaveHandler.cs
@@ -0,0 +1,25 @@
+//todo add logic
+using UnityEngine;
+
+public class WaveHandler : MonoBehaviour
+{
+    #region Vars/Props
+    public string sceneWin; //todo !
+	#endregion
+
+	#region Logic
+	#endregion
+
+	#region Component
+	private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+	#endregion
+}
